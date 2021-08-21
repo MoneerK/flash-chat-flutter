5930ea4009eb04f7d7f5597444a99ea815857b8c
MoneerK <moneer.kattan@gmail.com>
2021-08-21 12:44:56 +0300

    Add UI for diffrent users

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _firestore = FirebaseFirestore.instance;
-
+User loggedinuser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -15,7 +15,6 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  User loggedinuser;
   String messageText;
 
   @override
@@ -36,13 +35,7 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void messagesStream() async {
-    await for (var snapshots in _firestore.collection('messages').snapshots()) {
-      for (var message in snapshots.docs) {
-        print(message.data());
-      }
-    }
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -54,9 +47,8 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 //Implement logout functionality
-                messagesStream();
-                // _auth.signOut();
-                // Navigator.pop(context);
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -68,7 +60,6 @@ class _ChatScreenState extends State<ChatScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             MessagesStream(),
-
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -86,7 +77,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
-                      messageTextController.clear(); //Implement send functionality.
+                      messageTextController
+                          .clear(); //Implement send functionality.
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedinuser.email,
@@ -121,23 +113,29 @@ class MessagesStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.docs;
+        final messages = snapshot.data.docs.reversed;
         List<MessageBubble> messageBubbles = [];
         for (var message in messages) {
           //TODO extract text from data
           // final myText = (List) message.data()['text'];
-          final messageText = message.data().toString(); //text
+          final messageText = message.get('text'); //text
           //TODO extract text from data
-          final messageSender = message.data().toString(); //sender
-          final messageBubble =
-          MessageBubble(sender: messageSender, text: messageText);
+          final messageSender = message.get('sender'); //sender
+          final currentUser = loggedinuser.email;
+
+
+          final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+              isMe: currentUser == messageSender,
+          );
 
           messageBubbles.add(messageBubble);
         }
         return Expanded(
           child: ListView(
-            padding:
-            EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
             children: messageBubbles,
           ),
         );
@@ -146,20 +144,20 @@ class MessagesStream extends StatelessWidget {
   }
 }
 
-
-
 class MessageBubble extends StatelessWidget {
-  MessageBubble({this.sender, this.text});
+  MessageBubble({this.sender, this.text, this.isMe});
 
   final String sender;
   final String text;
+  final bool isMe;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
+      child:
+          Column(
+              crossAxisAlignment: isMe? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              children: <Widget>[
         Text(
           sender,
           style: TextStyle(
@@ -168,15 +166,23 @@ class MessageBubble extends StatelessWidget {
           ),
         ),
         Material(
-          borderRadius: BorderRadius.circular(30.0),
+          borderRadius:isMe? BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          ): BorderRadius.only(
+            topRight: Radius.circular(30.0),
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          ),
           elevation: 5.0,
-          color: Colors.lightBlueAccent,
+          color: isMe ?Colors.lightBlueAccent: Colors.white,
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
             child: Text(
               '$text',
               style: TextStyle(
-                color: Colors.white,
+                color: isMe? Colors.white: Colors.black54,
                 fontSize: 15.0,
               ),
             ),
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -45,11 +45,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
           children: <Widget>[
             Row(
               children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      child: Image.asset('images/logo.png'),
+                      height: 200.0,
+                    ),
                   ),
                 ),
                 TypewriterAnimatedTextKit(
